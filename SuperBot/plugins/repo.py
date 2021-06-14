59d2dd3a4e5b35ca69536a32720cc95f334bfc86
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-14 09:11:13 +0530

    Update repo.py

--- SuperBot/plugins/repo.py
+++ SuperBot/plugins/repo.py
@@ -1,55 +1,39 @@
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
+# SuperBot
+# made for SuperBot
+
+# imported from ULTRA-X by madboy482
+
 from telethon import events, Button, custom
 from SuperBot import bot
-from SuperBotX import xbot
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
+from SuperBot import assistant
+
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
-@xbot.on(events.InlineQuery(pattern=r"repo"))
+
+@assistant.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
- LEGENDX = event.builder
+ MadBoy = event.builder
  X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="obhai")]]
  query = event.text
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
- result = LEGENDX.article("UʟᴛʀᴀX",text="**UltraX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @UltraXOT**",buttons=X,link_preview=False)
- # updated by madboy482
- # updated by madboy482
- # updated by madboy482
+ result = MadBoy.article("SᴜᴘᴇʀBᴏᴛ",text="**SᴜᴘᴇʀBᴏᴛ's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @SuperBotOT**",buttons=X,link_preview=False)
  await event.answer([result])
-@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
-# updated by madboy482
-# updated by madboy482
-# updated by madboy482
+ 
+@assistant.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
 async def callback_query_handler(event):
-# inline by TEAMLEGEND, TEAMSuperBotX
-  await event.edit(text=f"**UʟᴛʀᴀX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @UltraXOT**",buttons=[
-   # updated by madboy482
-   # updated by madboy482
-   # updated by madboy482
-                [
-                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/SuperBot-OP/SuperBot-X"),
-                 # updated by madboy482
-                 # updated by madboy482
-                 # updated by madboy482
-                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/SuperBotXCHAT")],
-   # updated by madboy482
-   # updated by madboy482
-   # updated by madboy482
-                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FSuperBot-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FSuperBot-OP%2FHEROKU"),
-                     Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@legendx22/SuperBot-X#main.py"),
-                ]
-            ]
-                   # updated by madboy482
-                   # updated by madboy482
-                   # updated by madboy482
+  await event.edit(text=f"**SᴜᴘᴇʀBᴏᴛ's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @SuperBotOT**",buttons=[
+   [
+    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/MadBoy-X/SuperBot"),
+    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/SuperBot_Support")
+   ],
+   [
+    Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMadBoy-X%2FSuperBot-Deploy&template=https%3A%2F%2Fgithub.com%2FMadBoy-X%2FSuperBot-Deploy"),
+    Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@madboy482/SuperBot#main.py"),
+   ]
+  ]
                   )
+
+# SuperBot
+# made for SuperBot
+
+# imported from ULTRA-X by madboy482

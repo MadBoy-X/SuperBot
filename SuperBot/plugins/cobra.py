497136ef3848423979ef32501ad215935c492ca0
Pranav <83628215+Pranav18262@users.noreply.github.com>
2021-07-05 12:58:17 +0530

    bhkk

--- SuperBot/plugins/cobra.py
+++ SuperBot/plugins/cobra.py
@@ -54,7 +54,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid and query.startswith("Userbot"):
             rev_text = query[::-1]
             dc = paginate_help(0, CMD_LIST, "helpme")
-            result = builder.article("© Dark Super Bot Help",text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),buttons=dc,link_preview=False)
+            result = builder.article("© Team SuperBot  Help",text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),buttons=dc,link_preview=False)
             await event.answer([result] if result else None)
         else:
               reply_pop_up_alert = "Please get your own Userbot😁😁,for more info visit @Teamsuperbot😎😎"

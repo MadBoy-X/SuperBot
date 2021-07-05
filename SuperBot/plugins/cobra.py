06d63ba613b57588606391fc75809f3a94c22d23
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-05 13:23:54 +0530

    K

--- SuperBot/plugins/cobra.py
+++ SuperBot/plugins/cobra.py
@@ -183,7 +183,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
               else:
                     shortname = event.data_match.group(1).decode("UTF-8")
                     fcix = [custom.Button.inline("  𝕭𝖆𝖈𝖐 ",data="back({})".format(shivam)),custom.Button.inline(" 𝕮𝖑𝖔𝖘𝖊 ", data="close"),custom.Button.inline(" 𝕷𝖔𝖆𝖉 ",data="load({})".format(shivam_sh1vam))]
-                    reply_pop_up_alert = "Please get your own Userbot,for more info visit @TeamSuperBot
+                    reply_pop_up_alert = "Please get your own Userbot,for more info visit @TeamSuperBot"
                     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)#hehehe
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"back\((.+?)\)")))
    
